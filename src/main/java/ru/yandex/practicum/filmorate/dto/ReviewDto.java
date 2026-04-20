a89b43d6a82b1dba3a59a5887f45b139cc58481c
PerovII <perovii45@yandex.ru>
2026-04-20 20:54:41 +0500

    Исправление багов

--- src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
